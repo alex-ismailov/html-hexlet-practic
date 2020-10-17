85b0233c275ed87b2c486502c046a740438671c0
smlnx <alexander.is.dev@gmail.com>
2020-10-17 16:07:42 +0300

    fix(02-CSS-positioning/05-relative-and-absolute-positioning/__tests__/index.test.js) pass local jest tests

--- 02-CSS-positioning/05-relative-and-absolute-positioning/__tests__/index.test.js
+++ 02-CSS-positioning/05-relative-and-absolute-positioning/__tests__/index.test.js
@@ -1,28 +1,32 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import '@testing-library/jest-dom/extend-expect';
 
 import fs from 'fs';
 import { query } from '@github/query-selector';
 
-
 import path from 'path';
 
-console.log('+++++++++++++++++++++++++++');
-const myHtmlData = path.join(path.resolve(), '02-CSS-positioning/05-relative-and-absolute-positioning/index.html');
-const myCssData = path.join(path.resolve(), '02-CSS-positioning/05-relative-and-absolute-positioning/styles/app.css');
-console.log(myHtmlData);
-console.log(myCssData);
+// console.log('+++++++++++++++++++++++++++');
+// const myHtmlDataPath = path.join(path.resolve(), '02-CSS-positioning/05-relative-and-absolute-positioning/index.html');
+// const myCssDataPath = path.join(path.resolve(), '02-CSS-positioning/05-relative-and-absolute-positioning/styles/app.css');
+// console.log(myHtmlDataPath);
+// console.log(myCssDataPath);
+// const resMyHtmlDataPath = fs.readFileSync(myHtmlDataPath, 'utf-8');
+// console.log(resMyHtmlDataPath.toString()); // отдаль html, все ок
+// console.log('+++++++++++++++++++++++++++');
+// console.log(document);
+// console.log('+++++++++++++++++++++++++++');
 
 beforeAll(async () => {
   const styleData = await fs.promises.readFile(
     path.join(path.resolve(), '02-CSS-positioning/05-relative-and-absolute-positioning/styles/app.css'),
-    'utf-8',
   );
   const htmlData = await fs.promises.readFile(
     path.join(path.resolve(), '02-CSS-positioning/05-relative-and-absolute-positioning/index.html'),
-    'utf-8',
   );
-  // console.log('+++++++++++++++++++++++++++');
-  // console.log(htmlData);
 
   document.documentElement.innerHTML = htmlData.toString();
 
@@ -31,44 +35,44 @@ beforeAll(async () => {
   document.head.appendChild(styleEl);
 });
 
-// const getElementPropertyValue = (element, property) => {
-//   const elementStyle = getComputedStyle(element);
-//   return elementStyle.getPropertyValue(property);
-// };
+const getElementPropertyValue = (element, property) => {
+  const elementStyle = getComputedStyle(element);
+  return elementStyle.getPropertyValue(property);
+};
 
-// test('books-card to have relative position', () => {
-//   const element = query(document, '.books-card');
-//   const elementPosition = getElementPropertyValue(element, 'position');
+test('books-card to have relative position', () => {
+  const element = query(document, '.books-card');
+  const elementPosition = getElementPropertyValue(element, 'position');
 
-//   expect(elementPosition).toBe('relative');
-// });
+  expect(elementPosition).toBe('relative');
+});
 
-// test('book-description to have top and right rule', () => {
-//   const divBox = query(document, '.book-description');
-//   const divBoxTop = getElementPropertyValue(divBox, 'top');
-//   const divBoxRight = getElementPropertyValue(divBox, 'right');
+test('book-description to have top and right rule', () => {
+  const divBox = query(document, '.book-description');
+  const divBoxTop = getElementPropertyValue(divBox, 'top');
+  const divBoxRight = getElementPropertyValue(divBox, 'right');
 
-//   expect(divBoxTop).toBe('0px');
-//   expect(divBoxRight).toBe('0px');
-// });
+  expect(divBoxTop).toBe('0px');
+  expect(divBoxRight).toBe('0px');
+});
 
-// test('book-description to have 50% width', () => {
-//   const divBox = query(document, '.book-description');
-//   const divBoxWidth = getElementPropertyValue(divBox, 'width');
+test('book-description to have 50% width', () => {
+  const divBox = query(document, '.book-description');
+  const divBoxWidth = getElementPropertyValue(divBox, 'width');
 
-//   expect(divBoxWidth).toBe('50%');
-// });
+  expect(divBoxWidth).toBe('50%');
+});
 
-// test('book-description to have 100% height', () => {
-//   const divBox = query(document, '.book-description');
-//   const divBoxBottom = getElementPropertyValue(divBox, 'bottom');
+test('book-description to have 100% height', () => {
+  const divBox = query(document, '.book-description');
+  const divBoxBottom = getElementPropertyValue(divBox, 'bottom');
 
-//   expect(divBoxBottom).toBe('0px');
-// });
+  expect(divBoxBottom).toBe('0px');
+});
 
-// test('book-description to have position', () => {
-//   const divBox = query(document, '.book-description');
-//   const divBoxPosition = getElementPropertyValue(divBox, 'position');
+test('book-description to have position', () => {
+  const divBox = query(document, '.book-description');
+  const divBoxPosition = getElementPropertyValue(divBox, 'position');
 
-//   expect(divBoxPosition).toBe('absolute');
-// });
+  expect(divBoxPosition).toBe('absolute');
+});
